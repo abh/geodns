3f189ca0b044d6957181e773ba94b005b231b1e9
Ask Bjørn Hansen <ask@develooper.com>
2012-08-30 23:04:29 -0700

    Log goroutines count even without logging enabled

--- monitor.go
+++ monitor.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"log"
 	"runtime"
 	"time"
 )
 
 func monitor() {
 	for {
-		logPrintln("goroutines", runtime.NumGoroutine())
-		time.Sleep( 60 * time.Second)
+		log.Println("goroutines", runtime.NumGoroutine())
+		time.Sleep(60 * time.Second)
 	}
 }
