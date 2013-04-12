3a3c0425eaacb1785d746ec8f2f41f128b69811c
Ask Bjørn Hansen <ask@develooper.com>
2013-04-12 16:18:12 -0700

    Use a different HTTP port number for testing
    (I thought there was a function to find a free port, but I can't find it
    or an easy way to get the port number back when listening to port 0)

--- monitor_test.go
+++ monitor_test.go
@@ -18,6 +18,8 @@ var _ = Suite(&MonitorSuite{})
 func (s *MonitorSuite) SetUpSuite(c *C) {
 	s.zones = make(Zones)
 
+	*flaghttp = ":8881"
+
 	fmt.Println("Starting http server")
 
 	zonesReadDir("dns", s.zones)
@@ -28,12 +30,12 @@ func (s *MonitorSuite) SetUpSuite(c *C) {
 func (s *MonitorSuite) TestMonitorVersion(c *C) {
 	c.Check(true, DeepEquals, true)
 
-	res, err := http.Get("http://localhost:8053/version")
+	res, err := http.Get("http://localhost:8881/version")
 	c.Assert(err, IsNil)
 	page, _ := ioutil.ReadAll(res.Body)
 	c.Check(string(page), Matches, ".*<title>GeoDNS [0-9].*")
 
-	res, err = http.Get("http://localhost:8053/status")
+	res, err = http.Get("http://localhost:8881/status")
 	c.Assert(err, IsNil)
 	page, _ = ioutil.ReadAll(res.Body)
 	// just check that template basically works
